bd5f29882c8981de2c3f2810a1e7b9258dbbcc44
Rahat Santosh <rahatsantosh@gmail.com>
2020-10-07 18:11:19 +0530

    Add ImageFolder Dataset

--- datasets.py
+++ datasets.py
@@ -7,21 +7,34 @@ import transforms
 class MNIST(object):
     def __init__(self, batch_size, use_gpu, num_workers):
         transform = transforms.Compose([
+            transforms.Resize(size=(256, 256)),
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
         ])
 
         pin_memory = True if use_gpu else False
+        path = '/content/drive/My Drive/fellowship'
+        # data path
+        datapath = path
+        # veggie data path
+        data_folder = 'Data_07_preprocessing/training'
+        veggiedatapath = os.path.join(datapath,data_folder)
 
-        trainset = torchvision.datasets.MNIST(root='./data/mnist', train=True, download=True, transform=transform)
-        
+        trainset = torchvision.datasets.ImageFolder(
+            root=veggiedatapath,
+            transform=tf
+        )
+
         trainloader = torch.utils.data.DataLoader(
             trainset, batch_size=batch_size, shuffle=True,
             num_workers=num_workers, pin_memory=pin_memory,
         )
-        
-        testset = torchvision.datasets.MNIST(root='./data/mnist', train=False, download=True, transform=transform)
-        
+
+        testset = torchvision.datasets.ImageFolder(
+            root='/content/drive/My Drive/fellowship/new_test',
+            transform=tf
+        )
+
         testloader = torch.utils.data.DataLoader(
             testset, batch_size=batch_size, shuffle=False,
             num_workers=num_workers, pin_memory=pin_memory,
