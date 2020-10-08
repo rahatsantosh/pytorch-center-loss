ccfda59c4835a43f481e71ffdb207dbf63c994c0
Rahat Santosh <rahatsantosh@gmail.com>
2020-10-08 16:43:30 +0530

    Update Model

--- models.py
+++ models.py
@@ -1,48 +1,29 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from torchvision import models
 
 import math
 
-class ConvNet(nn.Module):
+class Identity(nn.Module):
+    """Identity Module"""
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def forward(self, x):
+        return x
+
+class Veggie_model(nn.Module):
     """LeNet++ as described in the Center Loss paper."""
     def __init__(self, num_classes):
         super(ConvNet, self).__init__()
-        self.conv1_1 = nn.Conv2d(1, 32, 5, stride=1, padding=2)
-        self.prelu1_1 = nn.PReLU()
-        self.conv1_2 = nn.Conv2d(32, 32, 5, stride=1, padding=2)
-        self.prelu1_2 = nn.PReLU()
-        
-        self.conv2_1 = nn.Conv2d(32, 64, 5, stride=1, padding=2)
-        self.prelu2_1 = nn.PReLU()
-        self.conv2_2 = nn.Conv2d(64, 64, 5, stride=1, padding=2)
-        self.prelu2_2 = nn.PReLU()
-        
-        self.conv3_1 = nn.Conv2d(64, 128, 5, stride=1, padding=2)
-        self.prelu3_1 = nn.PReLU()
-        self.conv3_2 = nn.Conv2d(128, 128, 5, stride=1, padding=2)
-        self.prelu3_2 = nn.PReLU()
-        
-        self.fc1 = nn.Linear(128*3*3, 2)
-        self.prelu_fc1 = nn.PReLU()
-        self.fc2 = nn.Linear(2, num_classes)
+        self.resnet = models.resnet50()
+        self.resnet.fc = Identity()
+        self.final = nn.Linear(in_features=2048, out_features=3, bias=True)
 
     def forward(self, x):
-        x = self.prelu1_1(self.conv1_1(x))
-        x = self.prelu1_2(self.conv1_2(x))
-        x = F.max_pool2d(x, 2)
-        
-        x = self.prelu2_1(self.conv2_1(x))
-        x = self.prelu2_2(self.conv2_2(x))
-        x = F.max_pool2d(x, 2)
-        
-        x = self.prelu3_1(self.conv3_1(x))
-        x = self.prelu3_2(self.conv3_2(x))
-        x = F.max_pool2d(x, 2)
-        
-        x = x.view(-1, 128*3*3)
-        x = self.prelu_fc1(self.fc1(x))
-        y = self.fc2(x)
+        x = self.resnet(x)
+        y = self.final(x)
 
         return x, y
 
